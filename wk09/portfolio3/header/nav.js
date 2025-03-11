973cd859cf08c51da626e69fc1d6d1aade6f5de9
ndeanon25 <nathan.deanon@evergreen.edu>
2025-03-11 16:53:13 -0700

    updates to wk09

---
+++ wk09/portfolio3/header/nav.js
@@ -0,0 +1,49 @@
+const{ AppBar, Toolbar, Typography, Button, Menu, MenuItem } = MaterialUI;
+const { useState } = React;
+
+
+
+
+//Navbar Component
+
+function Navbar() {
+
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    //Open nd close handlers for the dropdown menu
+    const handleMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="nav1" sx={{ flexGrow:1 }}>
+                    Portfolio
+                </Typography>
+                    <Button color="inherit" href="http://nad.csevg.com/wk09/portfolio3/index.html">Home</Button>
+
+                    <Button color="inherit" onClick={handleMenuOpen}>
+                    Projects &#9660;
+                    </Button>
+                    <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+                        
+
+                        <MenuItem onClick={handleMenuClose} component="a" href="http://nad.csevg.com/wk09/portfolio3/projects.html"> All Projects </MenuItem>
+                        <MenuItem onClick={handleMenuClose} component="a" href="http://nad.csevg.com/wk09/portfolio3/GameDex.html"> GameDex </MenuItem>
+                        <MenuItem onClick={handleMenuClose} component="a" href="http://nad.csevg.com/wk09/portfolio3/ImpossibleHangman.html"> Impossible Hangman </MenuItem>
+                        <MenuItem onClick={handleMenuClose} component="a" href="http://nad.csevg.com/wk09/portfolio3/ObjectDectection.html"> Object Dectection </MenuItem>
+
+                    </Menu>
+
+                    <Button color="inherit">About Me</Button>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+window.Navbar = Navbar; //Expose Navbar globally
